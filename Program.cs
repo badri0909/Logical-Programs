13a546dae9ea149da9ae1eaafb8d8344633e9373
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-09 09:43:03 +0530

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programs\n");
+
+            //Fibonacci.DisplayFibonacci();
+
+            //PerfectNumber.FindPerfectNumber();
+
+            //PrimeNumber.CheckPrimeNumber();
+
+            //ReverseNumber.ReverseTheNumber();
+
+           // CouponNumbers.GenerateCouponNumber();
+
+        }
+    }
+}
